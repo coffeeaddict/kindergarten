f8721b3e38ce654427c94e3eaffe7e663d2cb766
Hartog C. de Mik <hartog@organisedminds.com>
2014-03-17 20:29:18 +0100

    We must load more :)

--- lib/kindergarten.rb
+++ lib/kindergarten.rb
@@ -1,6 +1,12 @@
 require 'cancan/ability'
 require 'cancan/rule'
+require 'cancan/model_additions'
 require 'cancan/exceptions'
+require 'cancan/model_adapters/abstract_adapter'
+require 'cancan/model_adapters/default_adapter'
+require 'cancan/model_adapters/active_record_adapter' if defined? ActiveRecord
+require 'cancan/model_adapters/data_mapper_adapter' if defined? DataMapper
+require 'cancan/model_adapters/mongoid_adapter' if defined?(Mongoid) && defined?(Mongoid::Document)
 
 require 'active_support/core_ext'
 
